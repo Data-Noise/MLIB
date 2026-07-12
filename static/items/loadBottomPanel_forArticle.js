c32dde490090974e2a89517f3de39d9b49bee005
Data-Noise <124222217+Data-Noise@users.noreply.github.com>
2026-07-12 21:41:53 +0300

    Add files via upload

---
+++ static/items/loadBottomPanel_forArticle.js
@@ -0,0 +1,20 @@
+fetch('../items/bottomPanel.html')
+.then(response => response.text())
+.then(html => {
+	//document.getElementById('toolbar').innerHTML = html;
+const container = document.getElementById('bottomPanel');
+container.innerHTML = html;
+container.querySelectorAll('script').forEach(oldScript => {
+const newScript = document.createElement('script');
+// Копируем атрибуты и содержимое
+if (oldScript.src) {
+	newScript.src = oldScript.src;
+} else {
+	newScript.textContent = oldScript.textContent;
+}
+// Копируем другие атрибуты
+Array.from(oldScript.attributes).forEach(attr => {
+	newScript.setAttribute(attr.name, attr.value);
+});
+oldScript.parentNode.replaceChild(newScript, oldScript);
+});});
